f261096ff9886b29d02e6b0a4d7d6307fbb9df76
deadpool <118651379+DocStrange31@users.noreply.github.com>
2024-07-19 23:19:34 +0530

    Completed adding html elements required for the calculator

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function mul(a,b){
+    return a*b;
+}
+
+function div(a,b){
+    return a/b;
+}
+
+let num1,num2,op;
+
+function operate(op,num1,num2){
+    switch (op) {
+        case '+':
+            return(add(num1,num2));
+        case '-':
+            return(sub(num1,num2));
+        case 'x':
+            return(mul(num1,num2));
+        case '/':
+            return(div(num1,num2));
+        default:
+            break;
+    }
+}
