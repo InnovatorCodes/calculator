84c4d6eb66e0a11b3a1aed280e2b57af38242e98
deadpool <118651379+DocStrange31@users.noreply.github.com>
2024-07-21 01:26:16 +0530

    Partially added functionalities for buttons of calc
    Started adding functions to buttons of the calculator

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function div(a,b){
     return a/b;
 }
 
-let num1,num2,op;
+let num1=0,num2=0,op;
 
 function operate(op,num1,num2){
     switch (op) {
@@ -31,10 +31,45 @@ function operate(op,num1,num2){
     }
 }
 
+let numbuffer='';
+
 const clrbtn=document.querySelector('button.clr');
 const result=document.querySelector('.result');
 const input=document.querySelector('.input');
 clrbtn.addEventListener('click',()=>{
     result.textContent='';
-    input.textContent='';
+    input.textContent='0';
+    num1=0;
+    num2=0;
+    op='';
+    changenum=false;
+})
+
+let changenum=false;
+
+const btncontainer=document.querySelector('.buttons');
+btncontainer.addEventListener('click',(event)=>{
+    event=event.target;
+    if(event.classList.contains('num')){
+        numbuffer+=parseInt(event.textContent);
+        if(input.textContent==='0') input.textContent=event.textContent;
+        else input.textContent+=event.textContent;
+    }
+    else if(event.classList.contains('sym') && numbuffer.length){
+        if(!changenum){
+            num1=parseFloat(numbuffer);
+            changenum=!changenum;
+        } 
+        else if(changenum){
+            num2=parseFloat(numbuffer);
+            num1=operate(op,num1,num2);
+        } 
+        op=event.textContent;
+        if(op==='=') result.textContent=num1;
+        else input.textContent+=op;
+        numbuffer='';
+        console.log(changenum);
+        console.log(num1);
+    }
+    
 })
