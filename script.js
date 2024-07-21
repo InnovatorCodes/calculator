0435be1bb56d67fa75ad71cb9945541add20f1ca
deadpool <118651379+DocStrange31@users.noreply.github.com>
2024-07-22 01:50:16 +0530

    Completely rewrote code to handle corner cases well Rewritten code handles most cases of inputing numbers and performing operations well

--- script.js
+++ script.js
@@ -14,24 +14,29 @@ function div(a,b){
     return a/b;
 }
 
-let num1=0,op;
+let num=0,op;
 
-function operate(op,num1,num2){
+function operate(op,num,num2){
     switch (op) {
         case 'add':
-            return(add(num1,num2));
+            return(add(num,num2));
         case 'sub':
-            return(sub(num1,num2));
+            return(sub(num,num2));
         case 'mul':
-            return(mul(num1,num2));
+            return(mul(num,num2));
         case 'div':
-            return(div(num1,num2));
+            return(div(num,num2));
         default:
             break;
     }
 }
 
+function isSymbol(char){
+    return char==='+'||char==='-'||char==='&times'||char==='+&#247'||char==='%'
+}
+
 let numbuffer='';
+let startup=true;
 
 const clrbtn=document.querySelector('button.clr');
 const upper=document.querySelector('.upper');
@@ -39,46 +44,84 @@ const lower=document.querySelector('.lower');
 clrbtn.addEventListener('click',()=>{
     upper.textContent='';
     lower.textContent='0';
-    num1=0;
+    num=0;
     op='';
-    first=true;
+    startup=true;
     numbuffer='';
 })
 
-let first=true;
-
 const btncontainer=document.querySelector('.buttons');
+const dotbtn=btncontainer.querySelector('.num#dot');
+const bkspc=document.querySelector('#bckspc');
+
+bkspc.addEventListener('click',()=>{
+    if(!op){
+        numbuffer=lower.textContent;
+        numbuffer=numbuffer.slice(0,-1);
+        lower.textContent=numbuffer;
+        if(!numbuffer.length){
+            numbuffer='0'
+            lower.textContent='0';
+        }   
+    }
+});
+
 btncontainer.addEventListener('click',(event)=>{
     event=event.target;
     if(event.classList.contains('num')){
-        numbuffer+=parseInt(event.textContent);
-        lower.textContent=numbuffer;
-    }
-
-    else if(event.classList.contains('sym')){
-        if(!numbuffer.length){
-                if(!num1==='0') upper.textContent=upper.textContent.slice(0,-1)+event.textContent;
-                else if(upper.textContent.at(-1)==='=') upper.textContent=num1+event.textContent;
+        if(!(!numbuffer.length && event.textContent==0 && lower.textContent=='0')){
+            if(event.id=='dot' && lower.textContent=='0'|| numbuffer==''){
+                numbuffer='0.';
+                lower.textContent=numbuffer;
             } 
-        else if(event.id==='equals'){
-            upper.textContent+=numbuffer+'=';
-            num1=operate(op,num1,parseFloat(numbuffer))
-            lower.textContent=num1;
-            numbuffer='';
+            else{
+                numbuffer+=event.textContent;
+                lower.textContent=numbuffer;
+            }
+            if(event.id=='dot') dotbtn.disabled=true;
+        } 
+    }
+    else if(event.classList.contains('sym')){
+        dotbtn.disabled=false;
+        if(!numbuffer.length){
+            upper.textContent=num+event.textContent;
+            op=event.id;
         }
         else{
-            if(first){
-                num1=parseFloat(numbuffer);
-                first=!first;
-                upper.textContent=num1+event.textContent;
-            } 
-            else if(!first){
-                num1=operate(op,num1,parseFloat(numbuffer));
-                upper.textContent=num1+event.textContent;
-                lower.textContent=num1;
-            }   
+            if(startup){
+                if(!op){
+                    num=parseFloat(numbuffer);
+                    op=event.id;
+                } 
+                else if(op){
+                    num=operate(op,num,parseFloat(numbuffer));
+                    num=parseFloat(num.toFixed(4));
+                    op=event.id;
+                    lower.textContent=num;
+                }
+                upper.textContent=num+event.textContent;
+                numbuffer='';
+                startup=!startup;
+            }
+            else{
+                if(op) num=operate(op,num,parseFloat(numbuffer));   
+                console.log(num,op);
+                num=parseFloat(num.toFixed(4));
+                op=event.id;
+                lower.textContent=num;
+                upper.textContent=num+event.textContent;
+                numbuffer='';
+            }
         }
-        op=event.id;
-        numbuffer='';   
+    }
+    else if(event.classList.contains('res')){
+        if(numbuffer.length){
+            upper.textContent+=numbuffer;
+            num=operate(op,num,parseFloat(numbuffer));
+            num=parseFloat(num.toFixed(4));
+            lower.textContent=num;
+            upper.textContent+='=';
+            op='';
+        }
     }
 })
